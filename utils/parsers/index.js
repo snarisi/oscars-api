f8902da019e40086908471db8489ce029dd76811
Sam Narisi <snarisi@gmail.com>
2016-06-02 22:08:21 -0400

    refactored parsing into facory service

--- utils/parsers/index.js
+++ utils/parsers/index.js
@@ -1,77 +1,94 @@
 'use strict';
 
-const Parser = require('./parser.class');
-const BudgetParser = require('./budgetParser.class');
+const parser = require('./parser.service');
 const currencyTable = require('../helpers').currencyTable;
 const powerTable = require('../helpers').powerTable;
 const exchangeRateTable = require('../helpers').exchangeRateTable;
 
-// turn the budget into a number, representing the budget
-// in USD, converting other currencies accordingly
+module.exports = {
+	// turn the budget into a number, represented in
+	// USD, converting other currencies accordingly
+	parseBudget: function (str) {
+		if (str === null) return null;
 
-const parsers = {};
+		const cleanStr = parser.removeNoise(str);
+		const symbols = parser.getSymbols(cleanStr);
+		const numbers = parser.getNumbers(cleanStr);
+		const words = parser.getWords(cleanStr);
 
-parsers.parseBudget = function (str) {
-	if (str === null) return null;
+		let number, currency, exchangeRate, power, final;
 
-	const parser = new BudgetParser(str);
+		// in case of a range, the numbers array will have two numbers
+		// i decided to just pull the lower bound into the data set,
+		// but it would be easy to change this to take the higher bound,
+		// the average, or keep both numbers
+		number = numbers[0];
 
-	let currencyRate,
-			number,
-			power,
-			final;
+		// there's a chance the string could have other random
+		// symbols in it, so i'm taking the array and using the
+		// first one that matches one of the currency symbols.
+		// if none do, i default to USD
+		symbols.forEach(symbol => {
+			if (!currency && currencyTable[symbol]) {
+				currency = currencyTable[symbol];
+			}
+		});
+		if (!currency) currency = 'USD';
+		exchangeRate = exchangeRateTable[currency];
 
-	currencyRate = parser.getCurrencyRate();
-	number = parser.getNumber();
-	power = parser.getPower();
+		// likewise, there may be random words aside from the number-related
+		// ones, so i'm checking for a match in that array
+		words.forEach(word => {
+			if (!power && powerTable[word]) {
+				power = powerTable[word];
+			}
+		});
+		if (!power) power = 1;
 
-	final = Math.round(number * power * currencyRate);
+		final = Math.floor(number * power * exchangeRate);
 
-	// error checking
-	if (isNaN(final)) {
-		throw new Error('Got NaN');
-	}
-
-	return final;
-};
-
-// prints the budget in a readable, consistent format
-parsers.printBudget = function (num) {
-	if (num === null) return 'Not Available';
-
-	let str = num.toString();
-	let newStr = '';
-	let last = str.length - 1;
-
-	for (let i = last; i >= 0; i--) {
-		if (i < last && (last - i) % 3 === 0) {
-			newStr = ',' + newStr;
+		// if for some reason the answer is NaN, log it and return null
+		if (isNaN(final)) {
+			console.log('Got NaN from budget string: ', str);
+			return null;
 		}
-		newStr = str[i] + newStr;
+
+		return final;
+	},
+
+	// prints the budget in a readable, consistent format
+	printBudget: function (num) {
+		if (num === null) return 'Not Available';
+
+		let str = num.toString();
+		let newStr = '';
+		let last = str.length - 1;
+
+		for (let i = last; i >= 0; i--) {
+			if (i < last && (last - i) % 3 === 0) {
+				newStr = ',' + newStr;
+			}
+			newStr = str[i] + newStr;
+		}
+		return '$' + newStr;
+	},
+
+	// numbers here will be the year(s).  if there are two years,
+	// i join them with a slash. otherwise i just remove whitespace
+	// and uncecessary noise including the '(35th)' in every year.
+	// i kept the result as a string because that's the easiest way
+	// to handle the multi-year entries, but if it had to be a number
+	// for any reason, it would be easy to change this function to convert it
+	parseYear: function (str) {
+		let cleanStr = parser.removeNoise(str);
+		cleanStr = cleanStr.replace(/\s*\(.*\)\s*$/, '');
+		const numbers = parser.getNumbers(cleanStr);
+
+		return numbers.join('/');
+	},
+
+	// for the title just remove the noise
+	parseTitle: function (str) {
+		return parser.removeNoise(str);
 	}
-	return '$' + newStr;
 };
-
-// for the year just take out white space and any weird
-// wiki notation artifacts (keep them as strings and allow
-// them to represent multiple years)
-parsers.parseYear = function (str) {
-	const parser = new Parser(str);
-
-	let year = parser.consumeWhile(parser.isNum);
-	parser.skipWhiteSpace();
-	if (parser.peek() === '/') {
-		year += parser.get();
-		parser.skipWhiteSpace();
-		let otherYear = parser.consumeWhile(parser.isNum);
-		year += otherYear;
-	}
-	return year;
-};
-
-// for the title just take out the wiki citatation artifacts (eg, [1])
-parsers.parseTitle = function (str) {
-	return str.replace(/\s*\[.*\]\s*$/, '');
-};
-
-module.exports = parsers;
---
+++ utils/parsers/parser.service.js
@@ -0,0 +1,124 @@
+'use strict';
+
+// this module returns a parser object with some useful methods
+
+// private values and methods
+let ptr = 0;
+
+const getChar = function (str) {
+	return str[ptr++];
+};
+
+const peekChar = function (str) {
+	return str[ptr];
+};
+
+const eof = function (str) {
+	return peekChar(str) === undefined;
+};
+
+const consumeWhile = function (str, predicate) {
+	let returnString = '';
+	while (!eof(str) && predicate(peekChar(str))) {
+		returnString += getChar(str);
+	}
+	return returnString;
+};
+
+const skipWhiteSpace = function (str) {
+	return consumeWhile(str, char => /\s/.test(char));
+};
+
+const isNum = function (char) {
+	return /[0-9]|\.|,/.test(char);
+};
+
+const isNotNum = function (char) {
+	return !isNum(char);
+};
+
+const isLetter = function (char) {
+	return /[a-z]|[A-Z]/.test(char);
+};
+
+const isNotLetter = function (char) {
+	return !isLetter(char);
+};
+
+const isSymbol = function (char) {
+	return isNotNum(char) && isNotLetter(char) && !/\s/.test(char);
+};
+
+const isNotSymbol = function (char) {
+	return !isSymbol(char);
+};
+
+const getNextNumber = function (str) {
+	let numberString = '';
+
+	consumeWhile(str, isNotNum);
+
+	if (eof(str)) return null;
+
+	while (isNum(peekChar(str))) {
+		let char = getChar(str);
+		// skip commas
+		if (char !== ',') numberString += char;
+	}
+	return parseFloat(numberString);
+};
+
+const getNextSymbol = function (str) {
+	consumeWhile(str, isNotSymbol);
+	if (eof(str)) return null;
+	return consumeWhile(str, isSymbol);
+};
+
+const getNextWord = function (str) {
+	consumeWhile(str, isNotLetter);
+	if (eof(str)) return null;
+	return consumeWhile(str, isLetter);
+};
+
+
+// public methods
+module.exports = {
+	// removes wikipedia citation artifacts and
+	// extra parenthical info in the years
+	removeNoise: function (str) {
+		return str.replace(/\s*\[.*\]\s*$/, '');
+	},
+
+	// returns an array of all numbers inside the string
+	getNumbers: function (str) {
+		ptr = 0;
+		const numbers = [];
+		while (!eof(str)) {
+			let nextNum = getNextNumber(str);
+			if (nextNum) numbers.push(nextNum);
+		}
+		return numbers;
+	},
+
+	// returns an array of anything that isn't a letter or number
+	getSymbols: function (str) {
+		ptr = 0;
+		const symbols = [];
+		while (!eof(str)) {
+			let nextSymbol = getNextSymbol(str);
+			if (nextSymbol) symbols.push(nextSymbol);
+		}
+		return symbols;
+	},
+
+	// returns an array of words in the string
+	getWords: function (str) {
+		ptr = 0;
+		const words = [];
+		while (!eof(str)) {
+			let nextWord = getNextWord(str);
+			if (nextWord) words.push(nextWord);
+		}
+		return words;
+	}
+};
